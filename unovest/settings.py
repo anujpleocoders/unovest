569ebb1bc1a4a777bb222e28bffcc21313b3ca99
anujp.leocoders <anujp.leocoders@gmail.com>
2020-08-28 22:30:41 +0530

    anuj

--- unovest/settings.py
+++ unovest/settings.py
@@ -12,10 +12,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = ['unovest.herokuapp.com']
-
 
 # Application definition
 
@@ -42,10 +42,8 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
         # Heroku Static Files MIDDLEWARE
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
